ad2139de5a3cb3f58d9555ff2054983147f8edbf
American Computers <American Computers@LIGHTONRAY>
2021-12-10 01:12:04 +0200

    Add project files.

---
+++ Assignment3RemoveDuplicatesfromArray/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+namespace Assignment3RemoveDuplicatesfromArray
+{
+    class Program
+    {  
+        static void Main(string[] args)
+        {
+            int[] array = {1, 2, 2, 3, 3, 4, 1, 2, 5 };
+
+            // function to remove the duplicates
+            int[] distinct = array.Distinct().ToArray();
+
+            Console.WriteLine("The array without duplicates is: " + String.Join(",", distinct));
+        }
+    }
+}
